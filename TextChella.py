1d313d11519823d05f86343597c3b8a80502293f
Sanjeezy <stamizharasu@scu.edu.com>
2018-03-07 23:01:57 -0800

    added functions, added sms response

--- TextChella.py
+++ TextChella.py
@@ -1,29 +1,50 @@
 from pymongo import MongoClient
+from twilio.rest import Client
+
+username = 'ACbdc10bdbf54d948807715fde6396ad07' #Auth_SID
+password = '62bdcbe9eff5290f18df55601b33e9f9'	#Auth_Token
+client = Client(username, password)
 
 def get_connection():
-	user = "stamizharasu"
-	password = "Shyam1624abc"
+	user = "textchella"
+	password = "textchella123"
 	mlab_url = "ds261678.mlab.com:61678/textchella"
 	return MongoClient("mongodb://"+user+":"+password+"@"+mlab_url)
     
 
 def text_chella():
-    return 1
+	dbClient = get_connection()
+	db = dbClient['textchella']
+	result = db.ArtistInfo.aggregate(
+		[{'$sample' : {'size':1}}]
+	)
+
+	artist_name =""
+	artist_genre =""
+	artist_description =""
+	artist_link =""
+	doc_id =""
+
+	for artist in result:
+		artist_name =artist["ArtistName"]
+		artist_genre =artist["ArtistGenre"]
+		artist_description = artist["ArtistDescription"]
+		artist_link = artist["SpotifyLink"]
+		doc_id = artist["_id"]
+
+	users = db.Users.find()
+	for user in users:
+		client.messages.create(to=user["UserNumber"], from_="+16505390580", body=artist_name + artist_genre + artist_description + artist_link)
 
 def is_user(phone_number):
 	client = get_connection()
 	db = client['textchella']
-	result = db.Users.find()
-	#user_collection = client['textchella']['Users']
-	#user = user_collection.find({"UserNumber": phone_number})
-	#user = user_collection.find()
+	result = db.Users.find({"UserNumber": phone_number})
 
-	print(result.count())
-
-	# if user.count() == 0:
-	# 	return False
-	# else:
-	# 	return True
+	if result.count() == 0:
+		return False
+	else:
+		return True
     
 def subscribe(phone_number):
 	client = get_connection()
@@ -33,8 +54,4 @@ def subscribe(phone_number):
 	})
 
 if __name__ == '__main__':
-	print(is_user("+12038565701"))
-	#print(is_user("+54634634536"))
-	#subscribe("17264712648164")
-
-
+	text_chella()
--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, request, redirect, render_template
+from TextChella import is_user, subscribe
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/textChella")
+@app.route("/textChella", methods=['POST', 'GET'])
 def response():
     resp = MessagingResponse()
 
     incoming_number = request.form['From']
 
-    if is_user(incoming_number) == false:
-        subscribe_user(incoming_number)
-        resp.message(send_todays(incoming_number))
-        return str(resp)
+    if is_user(incoming_number) == False:
+        subscribe(incoming_number)
+        #resp.message(send_todays(incoming_number))
+        #return str(resp)
+        return ''
     else:
         return ''
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-    #hello
