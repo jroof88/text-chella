9b0e6fc0342ae3da4c2cb136f27924967f1cf175
Sanjeezy <stamizharasu@scu.edu.com>
2018-03-07 20:57:24 -0800

    Sanjay's first commit

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 @app.route("/textChella")
 def response():
     resp = MessagingResponse()
-    
+
     incoming_number = request.form['From']
-    
+
     #if is_user(incoming_number) == false:
         #subscribe_user(incoming_number)
         #resp.message(send_todays(incoming_number))
         #return str(resp)
     #else:
         #return ''
-        
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+    #hello
