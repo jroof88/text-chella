d8d1f5a28ffba03da89d2a711bcf84e7433a869e
Sanjeezy <stamizharasu@scu.edu>
2018-03-11 18:32:19 -0700

    added code to debug message

--- TextChella.py
+++ TextChella.py
@@ -27,8 +27,6 @@ def get_todays_artist():
 	result = db.ArtistInfo.find({
 		"Day": str(days)
 	})
-
-	print(result.count())
 	
 	artist_name = ""
 	artist_genre = ""
@@ -40,8 +38,11 @@ def get_todays_artist():
 		artist_genre = artist["ArtistGenre"]
 		artist_description = artist["ArtistDescription"]
 		artist_link = artist["SpotifyLink"]
-		
-	return "Good Morning!" + "\n\n" + days + " until the festival!" + "\n\n" + "Here is your song of the day!" + "\n\n" + "Artist Name: " + artist_name + "\n\n" + "Genre: " + artist_genre + "\n\n" + "Bio: " + artist_description + "\n\n" + "Featured Song: \n" + artist_link
+
+	message = "Good Morning!" + "\n\n" + days + " until the festival!" + "\n\n" + "Here is your song of the day!" + "\n\n" + "Artist Name: " + artist_name + "\n\n" + "Genre: " + artist_genre + "\n\n" + "Bio: " + artist_description + "\n\n" + "Featured Song: \n" + artist_link
+	
+	print(message)	
+	return message
 	
 def days_until_coachella():
 	date_of_chella = datetime.datetime(2018, 4, 14, 0, 0)
