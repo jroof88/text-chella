96ea701105251a61376f5d136ee8a9183e701eae
John Roof <jroof@scu.edu>
2018-03-11 18:09:29 -0700

    Pre-Heroku deploy updates

--- clock.py
+++ clock.py
@@ -2,8 +2,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=10, timezone='US/Pacific')
-def text_chella():
-    #text_chella()
+@sched.scheduled_job('cron', hour=18, minute=15, timezone='US/Pacific')
+def send_text():
+    text_chella()
     
 sched.start()
